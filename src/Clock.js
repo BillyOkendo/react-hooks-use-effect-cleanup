64b40820f73c347112d98e1bc9dd9590099ef1b7
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-07-20 21:51:47 +0300

    updated Clock.js

--- src/Clock.js
+++ src/Clock.js
@@ -1,13 +1,22 @@
+import { clearInterval } from "timers";
 import React, { useState, useEffect } from "react";
 
-function Clock() {
+function Clock(timerId) {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
     setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+
+
+    return function cleanup(){
+      clearInterval(timerId)
+    }
   }, []);
+
+  
 
   return <div>{time.toString()}</div>;
 }
